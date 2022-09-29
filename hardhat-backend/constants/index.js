eb2f815f2baacce45db34696639e5410b40d08b0
kkhitesh <hkowdiki@gmail.com>
2022-09-29 19:31:38 +0530

    Added NFT-Collection App

---
+++ hardhat-backend/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xbfc46d73732699774b8Df76a2fBb3450265A1dd6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
